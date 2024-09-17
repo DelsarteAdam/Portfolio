0a9b60609518b67eb53b4d5027cbe48ba37146d5
Adam <delsarte.adam@gmail.com>
2024-09-17 15:41:40 +0200

    design main page done

--- client/src/components/MainSection.tsx
+++ client/src/components/MainSection.tsx
@@ -6,6 +6,11 @@ function MainSection() {
       <section className={styles.text}>
         <p translate="no">DELSARTE</p>
         <p translate="no">ADAM</p>
+        <div className={styles.textLabel}>
+          <p>Web Dev</p>
+          <p>Graphist</p>
+          <p>Photographer</p>
+        </div>
       </section>
       <section className={styles.card}>
         <img src="/images/Auto_portrait_cut.webp" alt="portrait" />
